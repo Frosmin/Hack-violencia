c7ca62bcd4db33ca7ea15bf1f97404444cf079d2
link200309 <jonhh.0023@gmail.com>
2026-03-08 13:47:49 -0400

    chore: add cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 
@@ -6,7 +7,7 @@ const geminiRoutes = require('./routes/llmRoute');
 
 
 app.use(express.json());
-
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Servidor Express configurado correctamente.');
